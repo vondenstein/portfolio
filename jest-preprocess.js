dabc0db60063296e5f0d4f1e1e0d4746b1ea1896
Stephen Vondenstein <stephen@vondenstein.com>
2020-08-26 15:26:08 -0500

    delete unused tests

--- jest-preprocess.js
+++
@@ -1,6 +0,0 @@
-const babelOptions = {
-  presets: ["babel-preset-gatsby", "@emotion/babel-preset-css-prop"],
-  plugins: ["emotion"],
-}
-
-module.exports = require("babel-jest").createTransformer(babelOptions)
