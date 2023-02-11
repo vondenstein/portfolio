4f3b3991ad62eecfe548379031775cc38347908b
Stephen Vondenstein <stephen@vondenstein.com>
2023-02-10 20:41:07 -0700

    feat: added author link to posts (#74)
    * added author link to posts, moved main element to Layout
    * increment version

--- src/pages/code.tsx
+++ src/pages/code.tsx
@@ -4,7 +4,6 @@ import type { HeadFC, PageProps } from "gatsby"
 import Layout from "../components/Layout"
 import ProjectCard from "../components/ProjectCard"
 import { useProjects } from "../hooks/use-projects"
-import * as styles from "../styles/Page.module.css"
 import GitHubCard from "../components/GitHubCard"
 
 const CodePage: React.FC<PageProps> = () => {
@@ -12,29 +11,27 @@ const CodePage: React.FC<PageProps> = () => {
 
   return (
     <Layout>
-      <main className={styles.main}>
-        <h1>Software Projects</h1>
-        <p>
-          Here are a collection of projects I've worked on ~ for fun or to learn
-          ~ that I wanted to share.
-        </p>
-        {projects.edges.map(({ node }: Queries.ProjectsJsonEdge) => (
-          <ProjectCard
-            key={node.id}
-            id={node.id}
-            children={node.children}
-            internal={node.internal}
-            parent={node.parent}
-            title={node.title}
-            description={node.description}
-            icon={node.icon}
-            color={node.color}
-            links={node.links}
-          />
-        ))}
-        <h2>GitHub</h2>
-        <GitHubCard />
-      </main>
+      <h1>Software Projects</h1>
+      <p>
+        Here are a collection of projects I've worked on ~ for fun or to learn ~
+        that I wanted to share.
+      </p>
+      {projects.edges.map(({ node }: Queries.ProjectsJsonEdge) => (
+        <ProjectCard
+          key={node.id}
+          id={node.id}
+          children={node.children}
+          internal={node.internal}
+          parent={node.parent}
+          title={node.title}
+          description={node.description}
+          icon={node.icon}
+          color={node.color}
+          links={node.links}
+        />
+      ))}
+      <h2>GitHub</h2>
+      <GitHubCard />
     </Layout>
   )
 }
