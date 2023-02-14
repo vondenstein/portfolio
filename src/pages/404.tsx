8ea3bf9a6046d56eb0e7665e62c391540860e7cd
Stephen Vondenstein <stephen@vondenstein.com>
2023-02-13 18:54:17 -0700

    removed unnecessary styles

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,20 +6,6 @@ import Layout from "../components/Layout"
 import SEO from "../components/SEO"
 import Button from "../components/Button"
 
-const pageStyles = {
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
     <Layout>
