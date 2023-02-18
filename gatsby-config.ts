617ee6dc7fc19ec664b82edd8f0988ac1b241a6b
Stephen Vondenstein <stephen@vondenstein.com>
2023-02-18 16:21:47 -0600

    updated icon sizes and theme color (#86)

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -23,16 +23,68 @@ const config: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Vondenstein`,
+        name: `Stephen Vondenstein`,
         short_name: `Vondenstein`,
         description: `All the information you never wished you knew about Stephen.`,
         lang: `en`,
         start_url: `/`,
         background_color: `#111111`,
-        theme_color: `#fdf9f2`,
-        icon: `src/images/icon.png`,
+        theme_color: `#111111`,
         display: `standalone`,
-        cache_busting_mode: `none`,
+        cache_busting_mode: `name`,
+        icon: `src/images/icon.png`,
+        icons: [
+          {
+            src: "icons/icon-16x16.png",
+            sizes: "16x16",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "icons/icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
     },
     {
