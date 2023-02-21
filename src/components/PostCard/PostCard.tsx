e1f47cae0904ffbcd6c9478d031f1c008d9b93d5
Stephen Vondenstein <stephen@vondenstein.com>
2023-02-20 20:01:15 -0700

    made postcard responsive

--- src/components/PostCard/PostCard.tsx
+++ src/components/PostCard/PostCard.tsx
@@ -8,30 +8,24 @@ const PostCard = ({ id, excerpt, frontmatter }: Queries.Mdx) => {
   const image = getImage(frontmatter.hero_image)
 
   return (
-    <article key={id} style={{ marginBottom: "2rem" }}>
-      <Link to={`/blog/posts/${frontmatter.slug}`} className={styles.link}>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-          }}
-        >
+    <article key={id} className={styles.article}>
+      <Link to={`/blog/posts/${frontmatter?.slug}`} className={styles.link}>
+        <div className={styles.container}>
           <div>
             <GatsbyImage
               width={550}
               height={220}
               placeholder="blurred"
-              image={image}
-              alt={frontmatter.hero_image_alt}
+              image={image!}
+              alt={frontmatter?.hero_image_alt!}
               className={styles.image}
             />
           </div>
-          <div style={{ margin: "1rem 2rem 0", width: "480px" }}>
-            <h2 style={{ marginBottom: ".5rem" }}>{frontmatter.title}</h2>
-            <p style={{ marginBottom: ".5rem" }}>{excerpt}</p>
-            <p style={{ marginBottom: "0" }}>
-              {frontmatter.date} · 5 minute read
+          <div className={styles.text}>
+            <h2 className={styles.title}>{frontmatter?.title}</h2>
+            <p className={styles.excerpt}>{excerpt}</p>
+            <p className={styles.metadata}>
+              {frontmatter?.date} · 5 minute read
             </p>
           </div>
         </div>
