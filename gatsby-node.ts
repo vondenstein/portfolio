dfb368737c105e37c5689e75f755dd663cb9d7e5
Stephen Vondenstein <stephen@vondenstein.com>
2023-02-23 13:36:24 -0700

    implemented sitemap properties according to xml sitemap schema

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -1,7 +1,11 @@
+import { CreateNodeArgs, CreatePageArgs } from "gatsby"
 const readingTime = require(`reading-time`)
+const simpleGit = require(`simple-git`)
 
-exports.onCreateNode = ({ node, actions }) => {
+exports.onCreateNode = ({ node, actions }: CreateNodeArgs) => {
   const { createNodeField } = actions
+
+  // Add reading time estimates to blog posts (Mdx pages)
   if (node.internal.type === `Mdx`) {
     createNodeField({
       node,
@@ -9,4 +13,51 @@ exports.onCreateNode = ({ node, actions }) => {
       value: readingTime(node.body),
     })
   }
+}
+
+exports.onCreatePage = async ({ page, actions }: CreatePageArgs) => {
+  const { createPage } = actions
+
+  /***
+   * Add last modified date for sitemap using component name
+   * unless it is an mdx blog post, in which case use contentFilePath
+   ***/
+  const filePath = page.component.split("?__contentFilePath=").pop()
+  const fileLog = await simpleGit().log({
+    file: filePath,
+    maxCount: 1,
+    strictDate: true,
+  })
+
+  /***
+   * Add priority for sitemap
+   * 1.0 for homepage, all other priorities based on path depth
+   ***/
+  var priority = 1.0
+  if (page.path !== "/") {
+    const re = new RegExp("/", "g")
+    const depth = page.path.match(re)?.length!
+    priority = 1.0 - depth * 0.1
+  }
+
+  /***
+   * Add changefreq for sitemap - weekly by default
+   * monthly for blog posts, daily for blog index
+   ***/
+  var changeFreq = "weekly"
+  if (page.path.includes("/blog/posts/")) {
+    changeFreq = "monthly"
+  } else if (page.path === "/blog") {
+    changeFreq = "daily"
+  }
+
+  createPage({
+    ...page,
+    context: {
+      ...page.context,
+      priority: priority,
+      changeFreq: changeFreq,
+      lastMod: fileLog?.latest?.date,
+    },
+  })
 }
