ddf7569e22ee8d67002369f6fd6eecb9174c3579
Stephen Vondenstein <stephen@vondenstein.com>
2023-02-27 01:20:38 -0600

    fix: mobile navigation choppiness (#112)
    * fixed nav stuck open when resizing window
    * fixed mobile nav choppiness
    * fixed navigating home while navdrawer is open
    * increment version

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useContext } from "react"
 
 import * as styles from "./Layout.module.css"
 import NavBar from "../NavBar"
@@ -6,8 +6,24 @@ import "../../styles/global.css"
 import Footer from "../Footer"
 import PageContainer from "../PageContainer"
 import NavDrawer from "../NavDrawer"
+import MenuContext from "../MenuContext"
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const { isOpen, setOpen } = useContext(MenuContext)
+
+  const handleResize = (e: MediaQueryListEvent) => {
+    if (isOpen) setOpen(e.matches)
+  }
+
+  useEffect(() => {
+    const media = window.matchMedia("(max-width: 520px)")
+    media.addEventListener("change", handleResize)
+
+    return () => {
+      media.removeEventListener("change", handleResize)
+    }
+  })
+
   return (
     <>
       <NavDrawer />
