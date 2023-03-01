2e82578e24d210fcf4908ce7c1e18ba070e1bc49
Stephen Vondenstein <stephen@vondenstein.com>
2023-03-01 10:27:42 -0700

    updated background and work sections

--- src/pages/bio.tsx
+++ src/pages/bio.tsx
@@ -27,35 +27,50 @@ const BioPage: React.FC<PageProps> = () => {
         <h2 style={{ marginBottom: "15px" }}>{author.title}</h2>
         <p style={{ marginBottom: "5px" }}>{author.subtitle}</p>
       </Block>
-      <h2>Passions</h2>
+      <h2>Background</h2>
       <p>
-        I'm looking for new opportunities to learn and collaborate with other
-        talented engineers. Other than building software, I most enjoy
-        connecting with people who share my passion for engineering and clean
-        design.{" "}
+        I'm a software engineer who loves to build new things. I graduated from
+        Louisiana State Univerity (Geaux Tigers) with a degree in computer
+        science, and since then have been enjoying connecting with people who
+        share my passion for engineering and design.
       </p>
       <h2>Work</h2>
-      <p></p>
       <p>
-        I'm a computer science student at Louisiana State University with a
-        knack for software development and design. My favorite projects are
-        those that challenge me to build upon my problem-solving skills while
-        maintaining my chops as a developer. I've spent time working at a
-        startup where I helped build a AI-assisted image editing web
-        application. I've also gained experience building and training complex
-        neural networks in and outside of the classroom, as well as building and
-        deploying web applications with React.JS and GraphQL.{" "}
+        I'm currently working at{" "}
+        <a href="https://spglobal.com" title="S&P Global Website">
+          S&P Global
+        </a>{" "}
+        as an associate software engineer in the engineering solutions division.
+        My team is primarily responsible for building and maintaining an array
+        of internal tools to support a cutting-edge data insights platform,
+        though we do maintain some customer-facing services. Our tools and
+        services are used across the organization to increase developer
+        efficiency, reduce platform-breaking errors in production, and relibably
+        deliver customer data.
       </p>
       <p>
-        If you're looking for more in-depth information about my professional
-        work or would like to work together, please reach out on{" "}
+        Before S&P, I worked at Stephenson Stellar Corporation, where I was able
+        to contribute to advanced security systems - including a system to
+        detect and alert administrators in real-time about threats to satellite
+        communications. I've co-founded a startup where our small team
+        architected and built an AI-assisted image editing web application. I've
+        also gained experience building and training neural networks in and
+        outside of the classroom, as well as deploying scalable web services to
+        the cloud.
+      </p>
+      <p>
+        My favorite projects are those that challenge me to build upon my
+        problem-solving skills while maintaining my chops as a developer - and
+        I'm always interested in opportunities to scratch this itch. If you're
+        looking for more in-depth information about my professional work or
+        would like to work together, please reach out on{" "}
         <a
           href="https://linkedin.com/in/stephen-vondenstein/"
           title="Stephen Vondenstein on LinkedIn"
         >
           LinkedIn
         </a>
-        .
+        !
       </p>
       <h2>Hobbies</h2>
       <p>
