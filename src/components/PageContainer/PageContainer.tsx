148fa80f37aeca709f9162e54ab6622812696286
Stephen Vondenstein <stephen@vondenstein.com>
2023-03-08 09:46:38 -0700

    prevented mobile nav scrolling when open

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -1,10 +1,20 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect } from "react"
 import MenuContext from "../MenuContext"
 
 import * as styles from "./PageContainer.module.css"
 
 const PageContainer: React.FC<React.PropsWithChildren> = ({ children }) => {
   const { isOpen } = useContext(MenuContext)
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = "hidden"
+      document.body.style.height = "100vh"
+      document.body.style.position = "sticky"
+    } else {
+      document.body.style.cssText = ""
+    }
+  }, [isOpen])
 
   return <div className={isOpen ? styles.open : styles.closed}>{children}</div>
 }
