592090dd408ad1f40b99920aef495bd047c78741
Stephen Vondenstein <stephen@vondenstein.com>
2023-03-08 10:17:48 -0700

    fix mobile issue?

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -1,5 +1,10 @@
 import React, { useContext, useEffect } from "react"
 import MenuContext from "../MenuContext"
+// import {
+//   disableBodyScroll,
+//   enableBodyScroll,
+//   clearAllBodyScrollLocks,
+// } from "body-scroll-lock"
 
 import * as styles from "./PageContainer.module.css"
 
@@ -8,10 +13,12 @@ const PageContainer: React.FC<React.PropsWithChildren> = ({ children }) => {
 
   useEffect(() => {
     if (isOpen) {
+      // disableBodyScroll(document.body)
       document.body.style.overflow = "hidden"
-      document.body.style.height = "100vh"
+      document.body.style.height = "100%"
       document.body.style.position = "sticky"
     } else {
+      // enableBodyScroll(document.body)
       document.body.style.cssText = ""
     }
   }, [isOpen])
