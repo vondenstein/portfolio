ead16c66117c968c10c85846585e7999b885ad81
Stephen Vondenstein <stephen@vondenstein.com>
2023-03-08 13:48:47 -0700

    animted MenuButton

--- src/components/MenuButton/MenuButton.tsx
+++ src/components/MenuButton/MenuButton.tsx
@@ -1,14 +1,20 @@
-import React, { MouseEventHandler } from "react"
+import React, { MouseEventHandler, useContext } from "react"
 
 import * as styles from "./MenuButton.module.css"
+import MenuContext from "../MenuContext"
 
 type MenuButtonProps = {
   onClick?: MouseEventHandler
 }
 
 const MenuButton: React.FC<MenuButtonProps> = ({ onClick }) => {
+  const { isOpen } = useContext(MenuContext)
+
   return (
-    <div className={styles.menuButton} onClick={onClick}>
+    <div
+      className={`${styles.menuButton} ${isOpen ? styles.open : styles.closed}`}
+      onClick={onClick}
+    >
       <span className={styles.bar} />
       <span className={styles.bar} />
       <span className={styles.bar} />
