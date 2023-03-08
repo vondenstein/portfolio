9b4b3a2f34ec9f25881206d77725c44dbf47692b
Stephen Vondenstein <stephen@vondenstein.com>
2023-03-08 12:55:13 -0600

    fix: mobile nav scrolling (#123)
    * fix navdrawer close on width change
    * prevented mobile nav scrolling when open
    * increment version

--- src/components/PageContainer/PageContainer.tsx
+++ src/components/PageContainer/PageContainer.tsx
@@ -1,10 +1,18 @@
-import React, { useContext } from "react"
+import React, { useContext, useEffect } from "react"
 import MenuContext from "../MenuContext"
 
 import * as styles from "./PageContainer.module.css"
 
 const PageContainer: React.FC<React.PropsWithChildren> = ({ children }) => {
   const { isOpen } = useContext(MenuContext)
+
+  useEffect(() => {
+    if (isOpen) {
+      document.documentElement.style.overflow = "hidden"
+    } else {
+      document.documentElement.style.cssText = ""
+    }
+  }, [isOpen])
 
   return <div className={isOpen ? styles.open : styles.closed}>{children}</div>
 }
