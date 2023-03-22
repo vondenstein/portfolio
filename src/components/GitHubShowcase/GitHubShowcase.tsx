9a5218f4269c015907dbaa62df2fd7eb61828518
Stephen Vondenstein <stephen@vondenstein.com>
2023-03-22 15:37:45 -0600

    added contrast detection for GitHubShowcase

--- src/components/GitHubShowcase/GitHubShowcase.tsx
+++ src/components/GitHubShowcase/GitHubShowcase.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { useGitHubRepos } from "../../hooks/use-github-repositories"
+import { isBright } from "../../utils/is-bright"
 import Block from "../Block"
 import Button from "../Button"
 
@@ -12,6 +13,8 @@ const GitHubShowcase: React.FC = () => {
     <div>
       <div className={styles.cardContainer}>
         {repositories.map(({ node }: { node: Queries.GitHub_Repository }) => {
+          const background = node?.primaryLanguage?.color!
+
           return (
             <a
               href={node?.url}
@@ -20,20 +23,44 @@ const GitHubShowcase: React.FC = () => {
             >
               <div
                 style={{
-                  backgroundColor: `${node?.primaryLanguage?.color}`,
+                  backgroundColor: `${background}`,
                 }}
                 className={styles.contentContainer}
               >
                 <div className={styles.content}>
                   <div className={styles.top}>
-                    <h3>{node?.name}</h3>
-                    <div className={styles.badge}>
+                    <h3
+                      style={{
+                        color: `${
+                          isBright(background) ? "#3f3f3f" : "#fdf9f2"
+                        }`,
+                      }}
+                    >
+                      {node?.name}
+                    </h3>
+                    <div
+                      className={styles.badge}
+                      style={{
+                        color: `${
+                          isBright(background) ? "#3f3f3f" : "#fdf9f2"
+                        }`,
+                        backgroundColor: `${
+                          isBright(background)
+                            ? "rgba(63, 63, 63, 0.1)"
+                            : "rgba(253, 249, 242, 0.3)"
+                        }`,
+                      }}
+                    >
                       {node?.isArchived ? (
                         <>Archive</>
                       ) : (
                         <>
                           <img
-                            src={`/icons/star.svg`}
+                            src={
+                              isBright(background)
+                                ? "/icons/star-dark.svg"
+                                : "/icons/star.svg"
+                            }
                             alt="Star"
                             className={styles.icon}
                           />
@@ -42,7 +69,14 @@ const GitHubShowcase: React.FC = () => {
                       )}
                     </div>
                   </div>
-                  <p className={styles.description}>{node?.description}</p>
+                  <p
+                    style={{
+                      color: `${isBright(background) ? "#3f3f3f" : "#fdf9f2"}`,
+                    }}
+                    className={styles.description}
+                  >
+                    {node?.description}
+                  </p>
                 </div>
               </div>
             </a>
